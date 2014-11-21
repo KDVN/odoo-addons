25a52ba6c047a773d457bb450bfe10e5d0222182
Truong Quang Thanh <thanhacun@gmail.com>
2014-11-21 14:18:49 +0700

    Reporting

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -30,7 +30,9 @@
         'templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
-        'views/session_workflow.xml'
+        'views/session_workflow.xml',
+        'views/session_board.xml',
+        'reports.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
