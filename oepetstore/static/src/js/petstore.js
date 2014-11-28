8038e842fb99acf1bfb49e915068e4393e6ea4e6
Truong Quang Thanh <thanhacun@gmail.com>
2014-11-28 15:06:41 +0700

    Communicate with Odoo server

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -32,12 +32,22 @@ openerp.oepetstore = function(instance, local) {
     
     local.HomePage = instance.Widget.extend({
     	template: "HomePage",
-    	className: 'oe_petstore_homepage',
+    	//className: 'oe_petstore_homepage',
 
     	start: function() {
-    		this.colorInput = new local.ColorInputWidget(this);
-    		this.colorInput.on("change:color", this, this.color_changed);
-    		this.colorInput.appendTo(this.$el);
+    		return $.when(
+    				new local.PetToysList(this).appendTo(this.$('.oe_petstore_homepage_left')),
+    				new local.MessageOfTheDay(this).appendTo(this.$('.oe_petstore_homepage_right'))
+    				); 
+    		
+    		//var self = this;
+    		//var model = new instance.web.Model("oepetstore.message_of_the_day");
+    		//model.call("my_method", {context: new instance.web.CompoundContext()}).then(function(result){
+    		//	self.$el.append("<div>Hello " + result["hello"] + "</div>")
+    		//});
+    		//this.colorInput = new local.ColorInputWidget(this);
+    		//this.colorInput.on("change:color", this, this.color_changed);
+    		//this.colorInput.appendTo(this.$el);
     		//var products = new local.ProductsWidget(
     		//		this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
     		//products.appendTo(this.$el);
@@ -63,4 +73,40 @@ openerp.oepetstore = function(instance, local) {
     });
 
     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
+    
+    local.MessageOfTheDay = instance.Widget.extend({
+    	template: "MessageOfTheDay",
+    	start: function(){
+    		var self = this;
+    		return new instance.web.Model("oepetstore.message_of_the_day")
+    			.query(["message"])
+    			.order_by('-create_date', '-id')
+    			.first()
+    			.then(function(result){
+    				self.$(".oe_mywidget_message_of_the_day").text(result.message)
+    			});
+    	},
+    });
+    
+    local.PetToysList = instance.Widget.extend({
+    	template: "PetToysList",
+    	start: function(){
+    		var self = this;
+    		return new instance.web.Model("product.product")
+    			.query(['name', 'image'])
+    			.filter([['categ_id.name', '=', "Pet Toys"]])
+    			.limit(5)
+    			.all()
+    			.then(function(result){
+    				for (var i=0; i<result.length; i++){
+    					item = result[i];
+        				//console.log(item);
+        				//console.log(item.image);
+        				//console.log(item.name);
+        				self.$el.append(QWeb.render('PetToy', {item: item}));
+    				};
+    				
+    			});
+    	},
+    });
 }
