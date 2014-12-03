9347d5912d7e5f9fda601516d7ddd47e2663f65c
Truong Quang Thanh <thanhacun@gmail.com>
2014-12-03 09:48:46 +0700

    KDVN Website

--- oepetstore/petstore.py
+++ oepetstore/petstore.py
@@ -15,3 +15,5 @@ class product(models.Model):
     _inherit = "product.product"
 
     max_quantity = fields.Float(string="Maximum Quantity")
+    provider_latitude = fields.Text(string="Latitude")
+    provider_longitude = fields.Text(string="Longitude")
--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -90,6 +90,9 @@ openerp.oepetstore = function(instance, local) {
     
     local.PetToysList = instance.Widget.extend({
     	template: "PetToysList",
+    	events: {
+    		'click .oe_petstore_pettoy': 'selected_item'
+    	},
     	start: function(){
     		var self = this;
     		return new instance.web.Model("product.product")
@@ -97,16 +100,76 @@ openerp.oepetstore = function(instance, local) {
     			.filter([['categ_id.name', '=', "Pet Toys"]])
     			.limit(5)
     			.all()
-    			.then(function(result){
-    				for (var i=0; i<result.length; i++){
-    					item = result[i];
-        				//console.log(item);
-        				//console.log(item.image);
-        				//console.log(item.name);
-        				self.$el.append(QWeb.render('PetToy', {item: item}));
-    				};
-    				
+    			.then(function(results){
+    				_(results).each(function(item){
+    					self.$el.append(QWeb.render('PetToy', {item: item}));
+    				});
     			});
     	},
+    	
+    	selected_item: function(e){
+    		this.do_action({
+    			type: 'ir.actions.act_window',
+    			res_model: 'product.product',
+    			res_id: $(e.currentTarget).data('id'),
+    			views: [[false, 'form']],
+    		});
+    	},
     });
+    
+    local.FieldColor = instance.web.form.AbstractField.extend({
+    	events: {
+    		'change input': function(e){
+    			if (!this.get('effective_readonly')){
+    				this.internal_set_value($(e.currentTarget).val());
+    			};
+    		},
+    	},
+    	
+    	init: function(){
+    		this._super.apply(this, arguments);
+    		this.set("value", "");
+    	},
+    	
+    	start: function(){
+    		this.on("change:effective_readonly", this, function(){
+    			this.display_field();
+    			this.render_value();
+    		});
+    		this.display_field();
+    		return this._super();
+    	},
+    	
+    	display_field: function(){
+    		this.$el.html(QWeb.render("FieldColor", {widget: this}));
+    	},
+    	
+    	render_value: function(){
+    		if (this.get("effective_readonly")){
+    			this.$(".oe_field_color_content").css("background-color", this.get("value") || "#FFFFFF");
+    			console.log(this.get("value"));
+    		} else {
+    			this.$("input").val(this.get("value") || "#FFFFFF");
+    		};
+    	},
+    });
+    
+    instance.web.form.widgets.add('color', 'instance.oepetstore.FieldColor');
+    
+    local.WidgetCoordinates = instance.web.form.FormWidget.extend({
+        start: function() {
+            this._super();
+            this.field_manager.on("field_changed:provider_latitude", this, this.display_map);
+            this.field_manager.on("field_changed:provider_longitude", this, this.display_map);
+            this.display_map();
+        },
+        display_map: function() {
+            this.$el.html(QWeb.render("WidgetCoordinates", {
+                "latitude": this.field_manager.get_field_value("provider_latitude") || 0,
+                "longitude": this.field_manager.get_field_value("provider_longitude") || 0,
+            }));
+        }
+    });
+
+    instance.web.form.custom_widgets.add('coordinates', 'local.WidgetCoordinates');
 }
